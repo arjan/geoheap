c4aaa42ad1607c3c69f164f873bcfa87f407988b
Arjan Scherpenisse <arjan@scherpenisse.net>
2012-02-27 17:51:27 +0100

    Default back to roadmap

--- priv/www/js/project.js
+++ priv/www/js/project.js
@@ -11,7 +11,7 @@ $(function()
     var myOptions = {
         center: new google.maps.LatLng(52.310550, 4.957151),
         zoom: 13,
-        mapTypeId: google.maps.MapTypeId.TERRAIN //ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("map"),
                                   myOptions);
