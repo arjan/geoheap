14f7e03027d029dde7714c8101fa0275c172798f
rafe-copeland <rafe.copeland@gmail.com>
2012-04-02 18:16:52 +0300

    Changed google maps styles. Without testing I can almost guarantee that it won't work somehow...

--- priv/www/js/project.js
+++ priv/www/js/project.js
@@ -122,14 +122,213 @@ $(function()
     $(".do_timebar:first").timebar();
     var timebar = $(".do_timebar:first").data('timebar');
 
+//this --> should probably go in a seperate file
+        var StyledMapStyleset = [
+                      {
+                        featureType: "administrative",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "landscape.natural",
+                        elementType: "labels",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "landscape.man_made",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "poi.attraction",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "poi.business",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "poi.government",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "poi.medical",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "poi.place_of_worship",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "poi.school",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "poi.sports_complex",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "transit.line",
+                        elementType: "labels",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "transit.station.airport",
+                        elementType: "labels",
+                        stylers: [
+                          { lightness: 58 },
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "poi.park",
+                        elementType: "labels",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "transit.station.airport",
+                        stylers: [
+                          { lightness: 30 }
+                        ]
+                      },{
+                        featureType: "transit.station.rail",
+                        elementType: "labels",
+                        stylers: [
+                          { visibility: "simplified" }
+                        ]
+                      },{
+                        featureType: "road.highway",
+                        elementType: "labels",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "road.highway",
+                        stylers: [
+                          { visibility: "on" },
+                          { saturation: -100 },
+                          { lightness: 60 }
+                        ]
+                      },{
+                        featureType: "road.arterial",
+                        elementType: "geometry",
+                        stylers: [
+                          { visibility: "on" },
+                          { hue: "#2a00ff" },
+                          { saturation: -100 },
+                          { lightness: 100 }
+                        ]
+                      },{
+                        featureType: "road.local",
+                        elementType: "geometry",
+                        stylers: [
+                          { saturation: -98 },
+                          { lightness: 60 }
+                        ]
+                      },{
+                        featureType: "road.arterial",
+                        elementType: "labels",
+                        stylers: [
+                          { visibility: "on" },
+                          { saturation: -97 },
+                          { lightness: 70 }
+                        ]
+                      },{
+                        featureType: "road.highway",
+                        elementType: "labels",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "road.local",
+                        elementType: "labels",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "transit",
+                        elementType: "labels",
+                        stylers: [
+                          { visibility: "simplified" }
+                        ]
+                      },{
+                        featureType: "poi",
+                        elementType: "labels",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "water",
+                        stylers: [
+                          { saturation: 100 },
+                          { lightness: 15 }
+                        ]
+                      },{
+                        featureType: "landscape",
+                        elementType: "labels",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      },{
+                        featureType: "landscape.man_made",
+                        stylers: [
+                          { visibility: "on" }
+                        ]
+                      },{
+                        featureType: "landscape.natural",
+                        stylers: [
+                          { hue: "#00ff00" },
+                          { lightness: -15 },
+                          { saturation: 5 }
+                        ]
+                      },{
+                        featureType: "poi.park",
+                        stylers: [
+                          { visibility: "on" },
+                          { hue: "#00ff00" },
+                          { saturation: -10 }
+                        ]
+                      },{
+                      },{
+                        featureType: "poi.park",
+                        elementType: "labels",
+                        stylers: [
+                          { visibility: "simplified" }
+                        ]
+                      },{
+                        featureType: "water",
+                        elementType: "labels",
+                        stylers: [
+                          { visibility: "off" }
+                        ]
+                      }
+                    ];
+                    
+//up until here this <-- probably should go in a seperate file
+                    
+    var styledMap = new google.maps.StyledMapType(styledMapStyleset, {name:"floriade_radar"});
+
     var myOptions = {
         center: new google.maps.LatLng(52.310550, 4.957151),
         zoom: 13,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeID.ROADMAP, 'floriade_radar']
+        }
     };
     var map = new google.maps.Map(document.getElementById("map"),
                                   myOptions);
 
+    map.mapTypes.set('floriade_radar', styledMap);
+    map.sertMapTypeId('floriade_radar');
 
     var getState = function() {
         var c = map.getCenter();
